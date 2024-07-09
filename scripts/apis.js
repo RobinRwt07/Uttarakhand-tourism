cb7f56eae6169602560f0bf7d95884329238f6f3
Robin Singh <robinrwt85@gmail.com>
2024-07-09 17:53:10 +0530

    removed min-js.css file

--- scripts/apis.js
+++ scripts/apis.js
@@ -2,18 +2,18 @@
 const api_key = "46856b024a23a0bcfcf98c5194d9945e";
 
 export const url = {
-    currentWeather(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key} `;
-    },
-    forecast(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`
-    },
-    geo(query) {
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query},uttarakhand,india&limit=1&appid=${api_key}`;
-    }
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key} `;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`
+  },
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query},uttarakhand,india&limit=1&appid=${api_key}`;
+  }
 }
 
 
 export function kelvintoCelsius(kel) {
-    return (kel - 273.15)
+  return (kel - 273.15)
 }
